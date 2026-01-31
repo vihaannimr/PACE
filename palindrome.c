d9ece880810b594cd44d94b2d75c0c3704de395c
Vihan Ajay Nimr <vihaannimr@gmail.com>
2026-01-31 14:00:07 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,15 @@
+bool isPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+    long long reversed = 0;
+    int temp = x;
+
+    while (temp > 0) {
+        int digit = temp % 10;
+        reversed = reversed * 10 + digit;
+        temp /= 10;
+    }
+
+    return (reversed == x);
+}
